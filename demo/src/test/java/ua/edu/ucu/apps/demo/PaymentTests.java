375112751939a27e13e68fbee6a7ee1402cfd03c
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-10-26 15:01:29 +0300

    Update PaymentTests.java

--- demo/src/test/java/ua/edu/ucu/apps/demo/PaymentTests.java
+++ demo/src/test/java/ua/edu/ucu/apps/demo/PaymentTests.java
@@ -1,13 +1,9 @@
 package ua.edu.ucu.apps.demo;
-
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import ua.edu.ucu.apps.demo.payment.CreditCardPaymentStrategy;
 import ua.edu.ucu.apps.demo.payment.PayPalPaymentStrategy;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
 
 class PaymentTests {
     private PayPalPaymentStrategy payPalPayment;
@@ -19,16 +15,15 @@ class PaymentTests {
         payPalPayment = new PayPalPaymentStrategy();
         creditCardPayment = new CreditCardPaymentStrategy();
         price = 100.0;
-
     }
 
     @Test
     public void testCreditCard() {
-        assertEquals(price, creditCardPayment.pay(price));
+        Assertions.assertEquals(price, creditCardPayment.pay(price));
     }
 
     @Test
     public void testPayPal() {
-        assertEquals(price, payPalPayment.pay(price));
+        Assertions.assertEquals(price, payPalPayment.pay(price));
     }
 }
